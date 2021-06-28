9c6be0223cd569d79324bf6daa918da21ab1eec7
Mathias Lantean <ml180094@fi365.ort.edu.uy>
2021-06-28 00:40:06 -0300

    Se agregan metodos para crear, obtener y eliminar usuarios

--- Registry/app/controllers/user.controllers.js
+++ Registry/app/controllers/user.controllers.js
@@ -1,18 +1,63 @@
-const Note = require('../models/user.models.js');
+const User = require('../models/user.models.js');
 
 // Create and Save a new user
 exports.create = (req, res) => {
+    // Validate request
+    if(!req.body) {
+        return res.status(400).send({
+            message: "User content can not be empty"
+        });
+    }
 
+    // Create a user
+    const user = new User({
+        email: req.body.email, 
+        password: req.body.password
+    });
+
+    // Save user in the database
+    user.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the user."
+        });
+    });
 };
 
 // Retrieve and return all users from the database.
 exports.findAll = (req, res) => {
-
+    User.find()
+    .then(users => {
+        res.send(users);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving users."
+        });
+    });
 };
 
 // Find a single user with a userId
 exports.findOne = (req, res) => {
-
+    User.findById(req.params.userId)
+    .then(user => {
+        if(!user) {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.userId
+            });            
+        }
+        res.send(user);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.userId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving user with id " + req.params.userId
+        });
+    });
 };
 
 // Update a user identified by the userId in the request
@@ -22,5 +67,22 @@ exports.update = (req, res) => {
 
 // Delete a user with the specified userId in the request
 exports.delete = (req, res) => {
-
+    User.findByIdAndRemove(req.params.userId)
+    .then(user => {
+        if(!user) {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.userId
+            });
+        }
+        res.send({message: "User deleted successfully!"});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.userId
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete user with id " + req.params.userId
+        });
+    });
 };
--- Registry/app/routes/user.routes.js
+++ Registry/app/routes/user.routes.js
@@ -1,18 +1,18 @@
 module.exports = (app) => {
     const users = require('../controllers/user.controllers.js');
 
-    // Create a new Note
+    // Create a new User
     app.post('/users', users.create);
 
     // Retrieve all users
     app.get('/users', users.findAll);
 
-    // Retrieve a single Note with noteId
-    app.get('/users/:noteId', users.findOne);
+    // Retrieve a single User with userId
+    app.get('/users/:userId', users.findOne);
 
-    // Update a Note with noteId
-    app.put('/users/:noteId', users.update);
+    // Update a User with userId
+    app.put('/users/:userId', users.update);
 
-    // Delete a Note with noteId
-    app.delete('/users/:noteId', users.delete);
+    // Delete a User with userId
+    app.delete('/users/:userId', users.delete);
 }
